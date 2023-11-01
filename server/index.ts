ae049688f7b69733b222b97062b5f38d4521e464
esty <127044375+estieisa@users.noreply.github.com>
2023-11-01 13:51:56 +0200

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -33,6 +33,13 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://c-teen.vercel.app');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  // Add other necessary headers as needed
+  next();
+});
+
 
 app.use("/api/posts", require("./router/postsRout"));
 app.use("/api/users", require("./router/usersRout"));
