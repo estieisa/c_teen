a8da592eb40c37acc871607fd2f75b8e5e6d720c
esty <127044375+estieisa@users.noreply.github.com>
2023-11-01 13:43:05 +0200

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(
   cors({
     credentials: true,
-    origin: ['https://c-teen.vercel.app', 'http://localhost:3000'],
+    origin: ['https://c-teen.vercel.app', 'http://localhost:3000', 'https://c-teen.vercel.app/api/posts/getAllPosts'],
     maxAge: 2592000,
     allowedHeaders:"Access-Control-Allow-Origin"
   })
