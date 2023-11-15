ff1204a1517857c57cf31d82959727f46cd6936d
esty <127044375+estieisa@users.noreply.github.com>
2023-11-15 17:31:49 +0200

    Update Header.tsx

--- client/src/view/pages/mainPages/Header.tsx
+++ client/src/view/pages/mainPages/Header.tsx
@@ -71,6 +71,7 @@ export default function Header() {
   const [selectedComponent, setSelectedComponent] = useState<JSX.Element>(
     <SignIn/>
   );
+  console.log(selectedComponent.type.name)
   const [open, setOpen] = useState(false);
   const [clicked, setClicked] = useState(false);
   const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
